e56c9b9a4a3a8fdb59ccdac639ad39640747f970
Nilanjan <nilanjandaw@gmail.com>
2017-05-12 22:54:25 +0530

    minor bug fixes and support library version upgrades

--- app/src/main/java/debapriya/thunderstruck/com/cercatrovapersonnel/support/Constants.java
+++ app/src/main/java/debapriya/thunderstruck/com/cercatrovapersonnel/support/Constants.java
@@ -7,5 +7,5 @@ package debapriya.thunderstruck.com.cercatrovapersonnel.support;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://192.168.0.101:8000/";
+    public static final String BASE_URL = "http://192.168.43.88:8000/";
 }
